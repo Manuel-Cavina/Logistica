57b2f9c149a200169b364f957d63129323497d2a
Manuel Cavina <cavinapellism@gmail.com>
2026-03-17 00:58:28 -0300

    feature: Desarrollo del modulo de prisma

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+
+
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`API is running on port ${process.env.PORT ?? 3000}`);
+
   
 }
 bootstrap();
